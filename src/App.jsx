b4c52ae572283d9398f3f3c1ac29785414869838
CoderSid063 <sudhansupanda063@gmail.com>
2023-12-18 15:01:56 +0530

    feat: complete "Hero" section.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./app.scss";
 import Test from "./components/Test";
+import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
 const App = () => {
   return (
     <div>
       <section id="Homepage">
         <Navbar />
+        <Hero />
       </section>
       <section id="Services">Pareleal</section>
       <section>Service</section>
